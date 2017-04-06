369c6e7782e1610816bc82db249d41e4be425f4b
Nikitesh Bhadade <nikitesh@circldesign.com>
2017-04-06 15:13:57 +0530

    First CI Push

---
+++ application/views/test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html> 
+<html lang = "en"> 
+
+   <head> 
+      <meta charset = "utf-8"> 
+      <title>Nikitesh 1st Hello World view</title> 
+   </head>
+	
+   <body> 
+     <h1> Hello From Nikitesh</h1>
+   </body>
+	
+</html>
